e25e27f05a90526a3155dc478df613cdf653920d
Onyedibia <onyedibiaokeoma@gmail.com>
2023-09-10 07:26:58 -0700

    pushing all files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ *
+ * Return: (0) as final output
+ */
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
